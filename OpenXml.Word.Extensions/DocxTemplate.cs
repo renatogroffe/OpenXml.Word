25556cc58105f4a27149b076c9dd8b3f18a4f971
Renato Groffe <renato.groffe@yahoo.com.br>
2015-03-15 15:18:23 -0300

    Gerando arquivos .docx a partir de templates
    Exemplo demonstrando a geração de arquivos .docx a partir de templates.,
    a partir do uso do Open XML SDK.

---
+++ OpenXml.Word.Extensions/DocxTemplate.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+using DocumentFormat.OpenXml.Packaging;
+
+namespace OpenXml.Word.Extensions
+{
+    public static class DocxTemplate
+    {
+        public static void CriarNovoDocumento(
+            string caminhoTemplate,
+            string caminhoArquivoDestino,
+            Dictionary<string, string> substituicoes)
+        {
+            File.Copy(caminhoTemplate, caminhoArquivoDestino);
+
+            using (WordprocessingDocument wordDoc =
+                WordprocessingDocument.Open(caminhoArquivoDestino, true))
+            {
+                string docText = null;
+                using (StreamReader sr =
+                    new StreamReader(wordDoc.MainDocumentPart.GetStream()))
+                {
+                    docText = sr.ReadToEnd();
+                }
+
+                Regex regexText;
+                foreach (var itemSubstituicao in substituicoes)
+                {
+                    regexText = new Regex(itemSubstituicao.Key);
+                    docText = regexText.Replace(docText, itemSubstituicao.Value);
+                }
+
+                using (StreamWriter sw = new StreamWriter(
+                    wordDoc.MainDocumentPart.GetStream(FileMode.Create)))
+                {
+                    sw.Write(docText);
+                }
+            }
+        }
+    }
+}
---
+++ OpenXml.Word.Tests/DocxTemplateTeste01.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenXml.Word.Extensions;
+
+namespace OpenXml.Word.Tests
+{
+    [TestClass]
+    public class DocxTemplateTeste01
+    {
+        [TestMethod]
+        public void TestarGeracaoDocx()
+        {
+            Dictionary<string, string> substituicoes =
+                new Dictionary<string, string>();
+            substituicoes["#NOME_CLIENTE#"] =
+                "João da Silva";
+            substituicoes["#ENDERECO_CLIENTE#"] =
+                "Avenida Paulista, 950 - São Paulo - SP";
+            substituicoes["#NOME_ASSINATURA#"] =
+                "Pedro Oliveira";
+
+            string caminhoTemplate =
+                ConfigurationManager.AppSettings["CaminhoArquivoTemplate"];
+            string caminhoArquivoDestino =
+                ConfigurationManager.AppSettings["DiretorioGeracaoArquivoTeste"] +
+                "Teste_" + DateTime.Now.ToString("dd-MM-yyyy_HH'h'mm'min'ss's'") + ".docx";
+
+            DocxTemplate.CriarNovoDocumento(
+                caminhoTemplate,
+                caminhoArquivoDestino,
+                substituicoes);
+
+            Assert.IsTrue(File.Exists(caminhoArquivoDestino));
+        }
+    }
+}
